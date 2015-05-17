696d0d00046408cb63795d80245cbefa675a2991
Artur G <ontur00@gmail.com>
2015-05-17 12:38:47 +0200

    1) Intent  Play music UI Thread        a) GUI xml        b)test GUI

--- app/src/main/java/com/example/radog90/mymusic/PlayMusicUIThread.java
+++ app/src/main/java/com/example/radog90/mymusic/PlayMusicUIThread.java
@@ -1,11 +1,18 @@
 package com.example.radog90.mymusic;
 
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 /**
  * Wyglad oraz funkcje nowego okna
@@ -15,6 +22,8 @@ public class PlayMusicUIThread extends ActionBarActivity {
 
     private TextView textView1;
     private boolean log_i = true;                   //Zmienna do wyswietlania log.i(...);
+    private Button previousSong;
+    private ToggleButton playPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,9 @@ public class PlayMusicUIThread extends ActionBarActivity {
      * Metoda która inicjalizuje GUI
      */
     private void initializeGUI() {
+        //inicjalizujaca buttonow
+        initializeButtons();
+
         textView1 = (TextView) findViewById(R.id.Album_Name);
 
         //Pobierz zawartosc Resources
@@ -40,4 +52,87 @@ public class PlayMusicUIThread extends ActionBarActivity {
         textView1.setText(tabStringFromRes[0]);
     }
 
+    /**
+     * Metoda inicjalizujaca wszystkie buttony
+     */
+    private void initializeButtons() {
+        //Buton dla poprzedniej piosenki
+        previousSong = (Button) findViewById(R.id.previous_Song);
+
+        //Akcja dla buttona, zalaczenie poprzedniej piosenki
+        ButtonsListener listener = new ButtonsListener();
+        previousSong.setOnClickListener(listener);
+
+        //Akcja dla buttona przy długim kliknieciu
+        ButtonsLongClickListener longListener = new ButtonsLongClickListener();
+        previousSong.setOnLongClickListener(longListener);
+
+        //ToggleButton dodanie akcji Play/Pause muzyki
+        playPause = (ToggleButton) findViewById(R.id.play_Pause);
+        PlayPauseToggleListener playPauseToggleListener = new PlayPauseToggleListener();
+        playPause.setOnCheckedChangeListener(playPauseToggleListener);
+    }
+
+    /**
+     * implementacja Listenerow dla wszystkich button-ow
+     */
+    private class ButtonsListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+
+                case R.id.previous_Song:
+                    Toast.makeText(PlayMusicUIThread.this,
+                            "previous Song click", Toast.LENGTH_SHORT).show();
+                    MediaPlayer mediaPlayer = MediaPlayer.create(
+                            getBaseContext(), R.drawable.adb);
+                    mediaPlayer.start();
+                    break;
+            }
+        }
+    }
+
+    /**
+     * implementacja OnItemLongClickListener dla wszystkich button-ow
+     */
+    private class ButtonsLongClickListener implements View.OnLongClickListener {
+
+        @Override
+        public boolean onLongClick(View v) {
+
+            switch (v.getId()) {
+
+                case R.id.previous_Song:
+                    Toast.makeText(getApplicationContext(),
+                            "previous Song LONG CLICK", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    /**
+     * implementacja dla ToogleButtona
+     * ktory ma za zadanie Pause/Play muzyki
+     */
+    private class PlayPauseToggleListener implements CompoundButton.OnCheckedChangeListener {
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+
+            if (isChecked){
+                //Toggle Button ...
+                Toast.makeText(getApplicationContext(),
+                        String.valueOf(isChecked), Toast.LENGTH_SHORT).show();
+            }else{
+                //Toggle Button ...
+                Toast.makeText(getApplicationContext(),
+                        String.valueOf(isChecked), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
